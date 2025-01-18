6cd1482c64eb7470d78104a0256bb1b9ace73475
wanghanlin <wanghanlin917@163.com>
2025-01-18 23:50:20 +0800

    2025.1.

--- backend_wechat/settings.py
+++ backend_wechat/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+import os
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+UPLOAD_PATH = "upload/"
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
